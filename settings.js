ff2f8898dfac1c82321e7f6aaa0a06ad0a19c61d
Dassa20077 <pkayadassa@gmail.com>
2025-03-31 18:07:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     PREFIX: "DASSA-MD [KILL]>>>",
     SUCCESS_NUMBERS: ["94743277981"], 
-    SUCCESS_MESSAGE: "New session connected successfully!",
+    SUCCESS_MESSAGE: "*HELLO DASSA MD V2 OWNER 🚀*",
     CONNECTED_MESSAGE: `
 ┏━━━━━━━━━━━━━━
 ┃📍 *DASSA MD SESSION ID*
